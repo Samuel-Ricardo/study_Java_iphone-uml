df53c936d9128596ec468fd75264cbeddbf1d0df
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-13 14:03:43 -0300

    [:sparkles:] | update: class - InternetBrowser (impl)

--- src/impl/InternetBrowser.java
+++ src/impl/InternetBrowser.java
@@ -2,7 +2,12 @@ package impl;
 
 import interfaces.SmartApplication;
 
+import java.sql.SQLOutput;
+
 public class InternetBrowser implements SmartApplication {
+
+    private String pageURL;
+
     @Override
     public void goToHomeScreen(){
         System.out.println("Now in HomeScreen");
@@ -12,5 +17,18 @@ public class InternetBrowser implements SmartApplication {
     public void stop() {
         System.out.println("Stopping application");
         this.goToHomeScreen();
+    }
+
+    public void openPage(String url){
+        System.out.println("Opening page: "+url);
+        this.pageURL = url;
+    }
+
+    public void addNewGuide() {
+        System.out.println("Added new guide");
+    }
+
+    public void reloadPage() {
+        System.out.println("Reload page");
     }
 }
