848ea73e90ff3d4140f200813654200c46070044
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-13 14:06:08 -0300

    [:sparkles:] | create: class - Phone (impl)

---
+++ src/impl/Phone.java
@@ -0,0 +1,18 @@
+package impl;
+
+import interfaces.SmartApplication;
+
+public class Phone implements SmartApplication {
+
+    @Override
+    public void goToHomeScreen(){
+        System.out.println("Now in HomeScreen");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Stopping application");
+        this.goToHomeScreen();
+    }
+
+}
