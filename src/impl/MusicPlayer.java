d510f9ffc2bd0b47f41a19be6b202d5aa58f99b7
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-09-13 13:55:48 -0300

    [:sparkles:] | update: class - music player (impl)

--- src/impl/MusicPlayer.java
+++ src/impl/MusicPlayer.java
@@ -1,2 +1,33 @@
-package impl;public class MusicPlayer {
+package impl;
+
+import interfaces.SmartApplication;
+
+public class MusicPlayer implements SmartApplication {
+
+    private String selectedSong;
+
+    MusicPlayer(){}
+
+    @Override
+    public void goToHomeScreen(){
+        System.out.println("Now in HomeScreen");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Stopping application");
+        this.goToHomeScreen();
+    }
+
+    public void play() {
+        System.out.println("Play song");
+    }
+
+    public void pause() {
+        System.out.println("Paused");
+    }
+
+    public void select(String song){
+        System.out.println(song+" Selected");
+    }
 }
